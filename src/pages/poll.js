f878a8a76d714370ba640d2e3285d2fa96b91009
Stian Munkejord <smu027@uib.no>
2023-11-30 01:03:55 +0100

    Pretty finished product

--- src/pages/poll.js
+++ src/pages/poll.js
@@ -1,90 +1,139 @@
-import React, { useState, useEffect, useCallback } from 'react'; // Add useCallback
+import React, { useState, useEffect, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
 import NavBar from '../navbar';
 import '../style/poll.css';
 import Cookies from 'js-cookie';
+import { jwtDecode } from 'jwt-decode';
 
 function Poll() {
     const { id } = useParams();
+    
     const [data, setData] = useState([]);
+    const [userData, setUserData] = useState([]);
 
-    const handleClick = () => {
-        console.log(data);
-    }
+    const fetchPosts = useCallback(async () => {
+        try {
+            // Fetch poll data
+            const pollResponse = await fetch(`http://localhost:8080/poll/${id}`);
+            if (!pollResponse.ok) {
+                throw new Error('Poll data fetch failed');
+            }
+            const pollData = await pollResponse.json();
+            setData(pollData);
 
-    const fetchPosts = useCallback(() => { // Wrap fetchPosts with useCallback
-      fetch(`http://localhost:8080/poll/${id}`)
-        .then((response) => 
-          response.json())
-        .then(data => 
-          setData(data))
-    }, [id]); // Add id to the dependency array of useCallback
+            // Fetch user data
+            if (Cookies.get('token') !== "") {
+                const token = Cookies.get('token');
+                const decodedToken = jwtDecode(token);
+                const userName = decodedToken.name;
+  
+                const userResponse = await fetch(`http://localhost:8080/user/${userName}`, {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': token
+                    }
+                });
 
+                if (!userResponse.ok) {
+                    throw new Error('User data fetch failed');
+                }
+                const userData = await userResponse.json();
+                setUserData(userData);
+            } else {
+              console.log('No user data received');
+            }
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }, [id]);
 
-const postVote = (vote) => {
-  fetch('http://localhost:8080/vote', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': Cookies.get('token')
-    },
-    body: JSON.stringify(vote)
-  })
-  .then((response) => {
-    if (response.status === 403) {
-      throw new Error('You have allready voted on this poll');
-    } else if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then(data => console.log(data))
-  .catch(error => alert(error));
-}
+    const postVote = (vote) => {
+        let method = 'POST';
+        let hasVoted = false;
+        console.log("Methind is POSt")
+        if (Cookies.get('token') !== "") {
+          console.log("Not here")
+            
+
+            if (userData && userData.votes) {
+                for (const userVote of userData.votes) {
+                    if (userVote.poll.id === vote.poll.id) {
+                        hasVoted = true;
+                        break;
+                    } else if (userVote.poll.id === vote.poll.id) {
+						hasVoted = true;
+                        break;
+					}
+                }
+            }
+        }
+
+		if (hasVoted) {
+			method = 'PUT';
+		}
+		
+		console.log("Debug")
+		console.log(method);
+		console.log(vote);
+		console.log(userData)
+
+		fetch('http://localhost:8080/vote', {
+			method: method,
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': Cookies.get('token')
+			},
+			body: JSON.stringify(vote)
+		})
+		.then((response) => {
+			if (response.status === 403) {
+				throw new Error('You have already voted on this poll');
+			} else if (!response.ok) {
+				throw new Error(`HTTP error! status: ${response.status}`);
+			}
+			return response.text();
+		})
+		.then(data => console.log(data))
+		.catch(error => alert(error));
+        
+    };
 
     const handleSubmit = (e) => {
-      e.preventDefault();
-	    const answer = e.target.name === "yes";
-
-      const vote = {
-		  choice : answer,
-      poll : data
-	    }
-      console.log("Bruv");
-      console.log(data)
-      console.log(vote);
-      postVote(vote);
+        e.preventDefault();
+        const answer = e.target.name === "yes";
+        const vote = {
+            choice: answer,
+            poll: data
+        };
+        postVote(vote);
+        fetchPosts();
     };
 
     useEffect(() => {
-      fetchPosts();
+        fetchPosts();
     }, [fetchPosts]);
-
 
     return (
         <>
-        <NavBar />
-        <h1 className='title'>Poll {id}</h1>
-        <div className='content'>
-            <h2>{data.title}</h2>
-            <p className="question">{data.question}</p>
-            <p>This poll stated: {data.startDate}</p>
-            <p>This poll will end: {data.endDate}</p>
-            <div>
-                <p>Yes: {data.greenVotes}</p>
-                <p>No: {data.redVotes}</p>
+            <NavBar />
+            <h1 className='title'>Poll {id}</h1>
+            <div className='content'>
+                <h2>{data.title}</h2>
+                <p className="question">{data.question}</p>
+                <p>This poll started: {data.startDate}</p>
+                <p>This poll will end: {data.endDate}</p>
+                <div>
+                    <p>Yes: {data.greenVotes}</p>
+                    <p>No: {data.redVotes}</p>
+                </div>
+                <div>
+                    <button name="yes" onClick={handleSubmit}>Vote yes</button>
+                    <button name="no" onClick={handleSubmit}>Vote no</button>
+                </div>
+                {data && data.createdBy ? (<p>Created by: {data.createdBy.name}</p>) : (<p>Created by: Anonymous</p>)}
             </div>
-            <div>
-              	<button name="yes" onClick={handleSubmit}>Vote yes</button>
-            	<button name="no" onClick={handleSubmit}>Vote no</button>
-            </div>
-            {data && data.createdBy ? (<p>Created by: {data.createdBy.name}</p>) : (<p>Created by: Anonymous</p>)}
-            
-            <button onClick={handleClick}>printPoll</button>
-            <button onAbort={console.log(data)}>Test</button>
-        </div>
-        
         </>
     );
 }
+
 export default Poll;
